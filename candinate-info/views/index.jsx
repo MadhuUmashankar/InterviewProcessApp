6d926f4d18b088b22a1fbe5536b5136e96b3c181
Rani K <rani.k@SPI.COM>
2018-05-24 14:30:23 +0530

    commiting all files

--- candinate-info/views/index.jsx
+++ candinate-info/views/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -8,15 +7,4 @@ ReactDOM.render(
   <App url='http://localhost:3000/candinateInfo'
        pollInterval={2000}/>,
   document.getElementById('root')
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-
-ReactDOM.render(
-  <App url='http://localhost:3000/candinateInfo'
-       pollInterval={2000}/>,
-  document.getElementById('root')
->>>>>>> 816af9a91306e1702b527aaf7ae7488f5d282900
 );
